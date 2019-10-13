1e611f084c7d25ad2950c94fd04a548962f6ae73
Joanna Sowa <joanna.sowa19@gmail.com>
2019-10-13 15:06:01 -0500

    pushing current content

---
+++ app.js
@@ -0,0 +1,22 @@
+// variables //
+var questions = [];
+// array of questions (question-string, choices-array, answer-string, key-number) //
+// score //
+
+// helper functions //
+
+// events //
+// click highscore link //
+// find object in local storage //
+// parse object for screen //
+// display parsed object in html //
+
+// clear high scores //
+// go back //
+// start quiz //
+//
+// choose answer //
+// enter initials and submit score, go to highest score //
+
+// init //
+// none //
